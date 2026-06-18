fd02704b0aeed360cf952bbc6a8ee98d1b2fdc3c
Sunday <trenton.taylor.email@gmail.com>
2026-06-17 22:06:56 -0500

    sunday: Edit src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { SundayAnalyticsProvider } from './lib/sunday-analyzer';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SundayAnalyticsProvider siteKey="sa_f1519212695a8637fbb30c5b80427c55">
+      <App />
+    </SundayAnalyticsProvider>
   </StrictMode>
 );
